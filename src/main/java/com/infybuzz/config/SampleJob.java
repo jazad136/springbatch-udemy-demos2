a86cc80dc46db92d537d974d833afb9067238258
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-05-28 10:56:15 -0400

    Read in inputFile from command line arguments

--- src/main/java/com/infybuzz/config/SampleJob.java
+++ src/main/java/com/infybuzz/config/SampleJob.java
@@ -4,19 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -24,7 +22,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import com.infybuzz.model.StudentCsv;
 import com.infybuzz.writer.FirstItemWriter;
-
 @Configuration
 public class SampleJob {
 
@@ -65,16 +62,22 @@ public class SampleJob {
 	public Step firstChunkStep() { 
 		return new StepBuilder("First Chunk Step", jobRepository)
 				.<StudentCsv, StudentCsv>chunk(3, transactionManager)
-				.reader(flatFileItemReader())
+				.reader(flatFileItemReader(null))
 //				.processor(firstItemProcessor)
 				.writer(firstItemWriter)
 				.build();
 	}
 	
-	public FlatFileItemReader<StudentCsv> flatFileItemReader() { 
+	@StepScope
+	@Bean
+	public FlatFileItemReader<StudentCsv> flatFileItemReader(
+			@Value("#{jobParameters['inputFile']}") ClassPathResource classPathResource
+	) { 
 		FlatFileItemReader<StudentCsv> flatFileItemReader = 
 				new FlatFileItemReader<StudentCsv>();
-		flatFileItemReader.setResource(new ClassPathResource("InputFiles/students.csv"));
+//		flatFileItemReader.setResource(new ClassPathResource("InputFiles/students.csv"));
+		flatFileItemReader.setResource(classPathResource);
+
 		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() { 
 			{
 				setLineTokenizer(new DelimitedLineTokenizer() { 
