226318a259def61d277f3e69095efa085e23bc2c
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-05-28 06:51:30 -0400

    Create a CSV file model + sample CSV file. Remove non-essentials

--- src/main/java/com/infybuzz/writer/FirstItemWriter.java
+++ src/main/java/com/infybuzz/writer/FirstItemWriter.java
@@ -6,15 +6,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FirstItemWriter implements ItemWriter<Long> {
-
 	@Override
 	public void write(Chunk<? extends Long> items) throws Exception {
 		System.err.println("Inside Item Writer");
 		items.forEach(System.err::println);
 	}
-//	@Override
-//	public void write(List<? extends Long> chunk) throws Exception {
-//		
-//	}
-
 }
