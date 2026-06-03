3b33dc203006f85ae43e8e31bf9a5fa4b2351f02
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-03 15:46:24 -0400

    (test jdbcProcessor) Do not read property as first_name from database

--- spring-batch2-batch-service/src/main/java/com/infybuzz/model/StudentJdbc.java
+++ spring-batch2-batch-service/src/main/java/com/infybuzz/model/StudentJdbc.java
@@ -1,10 +1,7 @@
 package com.infybuzz.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class StudentJdbc {
 	private Long id;
-	@JsonProperty("first_name")
 	private String firstName;
 	private String lastName;
 	private String email;
