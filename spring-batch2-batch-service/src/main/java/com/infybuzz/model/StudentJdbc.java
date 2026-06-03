2bf172656daa798462d24c470643e548797ca24c
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-03 15:38:30 -0400

    Both StudentJdbc and StudentJson need @JsonProperty(first_name)

--- spring-batch2-batch-service/src/main/java/com/infybuzz/model/StudentJdbc.java
+++ spring-batch2-batch-service/src/main/java/com/infybuzz/model/StudentJdbc.java
@@ -1,7 +1,10 @@
 package com.infybuzz.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StudentJdbc {
 	private Long id;
+	@JsonProperty("first_name")
 	private String firstName;
 	private String lastName;
 	private String email;
