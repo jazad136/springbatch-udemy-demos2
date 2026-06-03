3765190fa3a5782b1e188619f1ce14a15ed2c5fb
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-03 15:46:55 -0400

    (test jdbcProcessor) Ignore unknown properties in StudentJson

--- spring-batch2-batch-service/src/main/java/com/infybuzz/model/StudentJson.java
+++ spring-batch2-batch-service/src/main/java/com/infybuzz/model/StudentJson.java
@@ -1,7 +1,9 @@
 package com.infybuzz.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class StudentJson {
 	private Long id;
 	@JsonProperty("first_name")
