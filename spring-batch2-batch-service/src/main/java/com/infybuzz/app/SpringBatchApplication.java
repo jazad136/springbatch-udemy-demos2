2e760d2d4230bd4f644bdd08a3cd157c24818668
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-06-08 15:35:55 -0400

    Add Enable batch processing annotation

--- spring-batch2-batch-service/src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ spring-batch2-batch-service/src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -1,5 +1,6 @@
 package com.infybuzz.app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 	"com.infybuzz.listener",
 	"com.infybuzz.controller"})
 @EnableAsync
+@EnableBatchProcessing
 public class SpringBatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchApplication.class, args);
